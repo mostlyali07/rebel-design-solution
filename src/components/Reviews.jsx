4cd689380851cd9e819f9fec4d76115fee37a2a4
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-05-15 18:02:17 +0500

    update

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -36,14 +36,14 @@ const Reviews = () => {
                         <div className="col-md-6 text-center">
                             <img src={Review_img} />
                         </div>
-                        <div className="col-md-6 rev-slide p-5">
+                        <div className="col-md-6 rev-slide">
                             <Slider {...settings}>
                                 <div>
                                     <div className="row">
-                                        <div className="col-md-6">
+                                        <div className="col-md-8">
                                             <h4>kate Roger - <span>Graphic Designer</span></h4>
                                         </div>
-                                        <div className="col-md-6">
+                                        <div className="col-md-4">
                                             <div className="stars text-end">
                                                 <FontAwesomeIcon icon={faStar} />
                                                 <FontAwesomeIcon icon={faStar} />
@@ -55,17 +55,19 @@ const Reviews = () => {
                                     </div>
                                     <div className="row">
                                         <div className="col-md-12">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
                                 <div>
                                     <div className="row">
-                                        <div className="col-md-6">
+                                        <div className="col-md-8">
                                             <h4>kate Roger - <span>Graphic Designer</span></h4>
                                         </div>
-                                        <div className="col-md-6">
+                                        <div className="col-md-4">
                                             <div className="stars text-end">
                                                 <FontAwesomeIcon icon={faStar} />
                                                 <FontAwesomeIcon icon={faStar} />
@@ -77,17 +79,19 @@ const Reviews = () => {
                                     </div>
                                     <div className="row">
                                         <div className="col-md-12">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
                                 <div>
                                     <div className="row">
-                                        <div className="col-md-6">
+                                        <div className="col-md-8">
                                             <h4>kate Roger - <span>Graphic Designer</span></h4>
                                         </div>
-                                        <div className="col-md-6">
+                                        <div className="col-md-4">
                                             <div className="stars text-end">
                                                 <FontAwesomeIcon icon={faStar} />
                                                 <FontAwesomeIcon icon={faStar} />
@@ -99,8 +103,10 @@ const Reviews = () => {
                                     </div>
                                     <div className="row">
                                         <div className="col-md-12">
-                                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            <p>
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                                            </p>
                                         </div>
                                     </div>
                                 </div>
