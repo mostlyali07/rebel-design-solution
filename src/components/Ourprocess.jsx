f96e41ff3431bd91fcac9fb90890233958b90b53
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-05-29 02:15:01 +0500

    update

--- src/components/Ourprocess.jsx
+++ src/components/Ourprocess.jsx
@@ -2,7 +2,7 @@
 const Ourprocess = () => {
     return (
         <>
-            <div className="container">
+            <div className="container py-5">
                 <div className="row">
                     <h5 className='fs-5 text-center pb-2'>
                         OUR SEO PROCESS
@@ -13,7 +13,67 @@ const Ourprocess = () => {
                     <p className="text-center pb-4">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat vitae commodi  <br /> explicabo culpa voluptate delectus veritatis quas cupiditate distinctio.
                     </p>
-                    
+                    <div className="col-md-1"></div>
+                    <div className="col-md-2">
+                        <div className="one text-center">
+                            <h6>
+                                1
+                            </h6>
+                            <h5>
+                                COMPETITOR <br />
+                                ANALYSIS
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="one text-center">
+                            <h6>
+                                2
+                            </h6>
+                            <h5>
+                                DEEP KEYWORDS <br />
+                                RESEARCH
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="one text-center">
+                            <h6>
+                                3
+                            </h6>
+                            <h5>
+                                ON PAGE <br />
+                                OPTIMIZATION
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="one text-center">
+                            <h6>
+                                4
+                            </h6>
+                            <h5>
+                                OFF PAGE <br />
+                                OPTIMIZATION
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="col-md-2">
+                        <div className="one text-center">
+                            <h6>
+                                5
+                            </h6>
+                            <h5>
+                                COMPREHENSIVE <br />
+                                REPORTING
+                            </h5>
+                        </div>
+                    </div>
+                    <div className="col-md-1"></div>
+                    <div className="d-flex justify-content-center my-5">
+                        <button className="btns-three" type="submit">Analyze My Site</button> &nbsp;&nbsp;
+                        <button className="btns-three" type="submit">Explore Site</button>
+                    </div>
                 </div>
             </div>
         </>
