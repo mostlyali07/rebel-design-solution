d6735b50f9b800774cebbd0229b29fb5960ba380
Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2023-05-29 15:27:47 +0500

    Update

--- src/components/Ourprocess.jsx
+++ src/components/Ourprocess.jsx
@@ -2,77 +2,79 @@
 const Ourprocess = () => {
     return (
         <>
-            <div className="container py-5">
-                <div className="row">
-                    <h5 className='fs-5 text-center pb-2'>
-                        OUR SEO PROCESS
-                    </h5>
-                    <h2 className='fw-bold text-center pb-2'>
-                        Lorem ipsum dolor sit amet consectetur
-                    </h2>
-                    <p className="text-center pb-4">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat vitae commodi  <br /> explicabo culpa voluptate delectus veritatis quas cupiditate distinctio.
-                    </p>
-                    <div className="col-md-1"></div>
-                    <div className="col-md-2">
-                        <div className="one text-center">
-                            <h6>
-                                1
-                            </h6>
-                            <h5>
-                                COMPETITOR <br />
-                                ANALYSIS
-                            </h5>
+            <div className="colr-bac">
+                <div className="container py-5">
+                    <div className="row">
+                        <h5 className='fs-5 text-center pb-2'>
+                            OUR SEO PROCESS
+                        </h5>
+                        <h2 className='fw-bold text-center pb-2'>
+                            Lorem ipsum dolor sit amet consectetur
+                        </h2>
+                        <p className="text-center pb-4">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia repellat vitae commodi  <br /> explicabo culpa voluptate delectus veritatis quas cupiditate distinctio.
+                        </p>
+                        <div className="col-md-1"></div>
+                        <div className="col-md-2">
+                            <div className="one text-center">
+                                <h6>
+                                    1
+                                </h6>
+                                <h5>
+                                    COMPETITOR <br />
+                                    ANALYSIS
+                                </h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-2">
-                        <div className="one text-center">
-                            <h6>
-                                2
-                            </h6>
-                            <h5>
-                                DEEP KEYWORDS <br />
-                                RESEARCH
-                            </h5>
+                        <div className="col-md-2">
+                            <div className="one text-center">
+                                <h6>
+                                    2
+                                </h6>
+                                <h5>
+                                    DEEP KEYWORDS <br />
+                                    RESEARCH
+                                </h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-2">
-                        <div className="one text-center">
-                            <h6>
-                                3
-                            </h6>
-                            <h5>
-                                ON PAGE <br />
-                                OPTIMIZATION
-                            </h5>
+                        <div className="col-md-2">
+                            <div className="one text-center">
+                                <h6>
+                                    3
+                                </h6>
+                                <h5>
+                                    ON PAGE <br />
+                                    OPTIMIZATION
+                                </h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-2">
-                        <div className="one text-center">
-                            <h6>
-                                4
-                            </h6>
-                            <h5>
-                                OFF PAGE <br />
-                                OPTIMIZATION
-                            </h5>
+                        <div className="col-md-2">
+                            <div className="one text-center">
+                                <h6>
+                                    4
+                                </h6>
+                                <h5>
+                                    OFF PAGE <br />
+                                    OPTIMIZATION
+                                </h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-2">
-                        <div className="one text-center">
-                            <h6>
-                                5
-                            </h6>
-                            <h5>
-                                COMPREHENSIVE <br />
-                                REPORTING
-                            </h5>
+                        <div className="col-md-2">
+                            <div className="one text-center">
+                                <h6>
+                                    5
+                                </h6>
+                                <h5>
+                                    COMPREHENSIVE <br />
+                                    REPORTING
+                                </h5>
+                            </div>
                         </div>
-                    </div>
-                    <div className="col-md-1"></div>
-                    <div className="d-flex justify-content-center my-5">
-                        <button className="btns-three" type="submit">Analyze My Site</button> &nbsp;&nbsp;
-                        <button className="btns-three" type="submit">Explore Site</button>
+                        <div className="col-md-1"></div>
+                        <div className="d-flex justify-content-center mt-5">
+                            <button className="btns-three" type="submit">Analyze My Site</button> &nbsp;&nbsp;
+                            <button className="btns-three" type="submit">Explore Site</button>
+                        </div>
                     </div>
                 </div>
             </div>
