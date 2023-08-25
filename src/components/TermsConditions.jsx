7b51b397c17fe3627918200db7e64fc211e17333
mostlyali07 <asyed7477@gmail.com>
2023-08-25 17:05:16 +0500

    update

--- src/components/TermsConditions.jsx
+++ src/components/TermsConditions.jsx
@@ -77,7 +77,7 @@ const TermsConditions = () => {
                         <div className="col-md-8">
                             <div className='text-center pt-5 mt-5'>
                                 <h1 className='fw-bold'>
-                                    Privacy <span style={{ color: '#222222' }}>Policy</span>
+                                    Terms & <span style={{ color: '#222222' }}>Conditions</span>
                                 </h1>
                                 <h2>
                                     Welcome to Rebel Design Solution
@@ -93,11 +93,112 @@ const TermsConditions = () => {
                 <div className="row">
                     <div className="col-md-2"></div>
                     <div className="col-md-8">
-                       
-                    </div>
+                        <p>
+                            The terms and conditions section of Rebel Design Solution draws out rules concerning using the Rebel Design Solution website and the services we provide to our clients. It also explains the essential criteria for refunds and the money-back guarantee policy.</p><p>
+                        </p>
+                        <p>
+                            Using the Rebel Design Solution's website, users invariably accept all the following obligations.
+                        </p>
+                        <h2>Search Rebel Design Solution Content</h2>
+                        <p>
+                            Rebel Design Solution owns the images, videos, corporate logo, landing pages, icons, footer, header, blogs, and any other content on this website. No individual or group of individuals may reproduce, reuse, edit, republish, or disseminate this information on any platform without first obtaining explicit written permission from Rebel Design Solution.</p >  <p>
+                            Illegal exploitation of our content violates trademark, copyright, and other applicable legal areas. Rebel Design Solution is fully authorized by law to pursue civil and criminal penalties against anybody who engages in the unlawful use of our website's material.</p >
+                        <h2>Refund Policy</h2>
+                        <h3>Please read through our below-provided refund policy for your reference and information.</h3> <p>
+                            It should be reminded that our return policy is null and void in the following circumstances:
+                        </p >
+                        <p>
+                            1. If a client availed a package that was discounted/marked down from the original cost.
+                        </p >
+                        <p>
+                            2. Domain registration, search engine optimization, web hosting services, and social media management services are not refundable under any circumstances.
+                        </p >
+                        <p>
+                            3. Projects delivered on an accelerated basis are non-refundable and are not eligible for refunds under any circumstances.
+                        </p >
+                        <p>
+                            4. If the customer has requested changes to the initial design concepts.
+                        </p >
+                        <p>
+                            5. If the cancellation is made for reasons other than the organization, this shall render the project non-refundable.
+                        </p >
+                        <p>
+                            6. If the customer has engaged another business to do the identical task we were hired for.
+                        </p >
+                        <p>
+                            7. If the client has in any way broken the company's policies.
+                        </p >
+                        <p>
+                            8. If the client does not reply to being approached by the firm after 14 days, i.e., is non-responsive or is not in communication with the organizational representative for more than 14 days.</p >  <p>
+                            9. If there was insufficient/incomplete feedback in the first creative brief.</p >
+                        <p>
+                            10. If a thorough redesign of the design or concept is required.</p >
+                        <p>
+                            11. If a refund request has crossed the stipulated timeframe (60 days) for doing so.</p >
+                        <p>
+                            12. If a client is unsatisfied with a certain service in a subscription bundle package of services, they are only eligible to receive the money related to that specific item in the bundle. In this instance, a refund of the full bundle package is not eligible.</p >
+                        <p>
+                            13. 'Change of opinion' or 'internal organizational incongruity' are unacceptable reasons to request a refund.</p >
+                        <p>
+                            14. If a company/organization/business changes its official name by amending, editing, or otherwise.</p >
+                        <p>
+                            15. In the case of full and final delivery of final files already being sent to the client, they will be ineligible to process a refund request.</p >
+                        <p>
+                            16. Complimentary services we provide are not subject to being processed for a refund.</p >
+                        <p>
+                            17. In the event that the client approves the first design concept.</p >
+                        <p>
+                            18. Work subcontracted out to a third party is not applicable for a refund request.</p >
+                        <p>
+                            19. Refund claims will not be entertained/processed in logo development if designs have been accepted and rendered as final files for final delivery.</p >
+                        <p>
+                            20. Refunds are not available for website design, development, or deployment once the customer has submitted the initial design concept approval passed to the web development team.</p >
+                        <p>
+                            21. If a customer availed any sort of payment plan for a service or package.</p ><p><strong>KEYNOTE:</strong></p>
+                        <p>
+                            At its sole discretion, the organization maintains the right to suspend, terminate, or reject any project without assigning a reason or providing prior notice. Following cancellation/dispute, the customer loses all rights to the created content for use and exhibition. After such an instance, all designs shall become the organization's sole property, which shall then be the sole rightful owner of the developed materials.</p >
+                        <h3>Claiming A Refund</h3>
+                        <p>
+                            To begin a refund request, please send an email to info@rebeldesignsolution.com. After your refund request initiation, our Billing and Compliance Department will contact you for refund request evaluation and processing. Following the conclusion of the initial review, your refund request process will begin.</p >
+                        <h3>Money-Back Guarantee</h3>
+                        <p>
+                            Rebel Design Solution follows a hassle-free 100% money-back guarantee policy concerning its SEO / SMM projects. However, we are pointing out some circumstances that may apply to our 100% money-back guarantee policy.</p > <p>
+                            Assume there is any obligation we cannot fulfill within the specified time frame. Rebel Design Solution will not be obligated to keep the customer's money and will repay the whole amount, including any transaction adjustments, to the client.</p > <p>
+                            Clients must begin the money-back guarantee process within 30 days of project completion. Requests received after 30 days will be ignored.</p ><h3>Payment Policy</h3><p>
+                            All digital marketing services provided by Rebel Design Solution, including Search Engine Optimization (SEO), Pay Per Click Advertising (PPC), Social Media Marketing (SMM), Content Marketing, and other similar services will be invoiced on a monthly recurring or Renewal basis. Customers may be charged on a regular monthly payment basis for particular web development and design projects, depending on the duration and complexity of the project.</p ><h3>Client Feedback And Approval</h3><p>
+                            All Rebel Design Solution projects are subject to clients' written feedback and approval. If a customer does not offer feedback within 30 days or by completing a project (whichever comes first), Rebel Design Solution reserves the right to keep the project fees. Clients cannot object to a full refund in this situation. However, a partial refund could be possible upon mutual agreement.</p ><h2>DISCLAIMERS AND LIMITATIONS OF LIABILITY</h2><h3>Client Websites</h3>
+                        <p>
+                            Rebel Design Solution is not liable for any damages caused to clients or their property (such as websites). Clients who are unhappy with our services may request a refund and stop using the website. Rebel Design Solution will not be liable for penalties for accidental leakage or loss of client data from the search engine.</p >
+                        <p>
+                            Rebel Design Solution employs white-hat SEO tactics that follow the most recent search engine rules. We only take action on customers' websites with their prior consent.
+                        </p >
+                        <h3>Changes To the Client's Website</h3>
+                        <p>
+                            All of our clients should back up their websites since it will be helpful in the event of data loss. We also highly advise our clients to modify their websites after first alerting us.
+                        </p >
+                        <h3>Changes To Website META Tags</h3>
+                        <p>
+                            Changes to the website's design, template, or META tags may impact both traffic and ranking. As a result, our clients are advised to change META information or modify the website and its content before gaining our trust. Rebel Design Solution is not responsible for any damages to ranking or traffic caused by changes in META information, design, theme, or content made by any party.</p ><h3>Search Engine Penalties</h3> <p>
+                            Rebel Design Solution is not liable if the client's website gets penalized by search engines for any reason. We are staunch supporters of white-hat SEO strategies that adhere to search engine standards. Furthermore, we notify clients before making any modifications to the website.</p ><h3>Web Development Policy</h3> <p>
+                            Rebel Design Solution takes on web development projects at the request of clients. We shall not bear any liability for data theft or any damage to online or offline property. We prefer to have user-level access to clients' websites rather than administrator access wherever feasible.</p >  <p>
+                            Rebel Design Solution also advises clients to update their website login credentials as soon as the project is completed. At the same time, we advise our clients to backup their websites on a reliable and secure server. If a website is waived off from the Google index or gets erased unintentionally, Rebel Design Solution will not be held accountable.</p ><h3>SEO Work</h3> <p>
+                            Rebel Design Solution will not be liable if Google discounts website links from the index. We shall also not be held liable if our clients' rankings are reduced due to search engine changes. Client performance concerns connected to ISP providers, hosting, and so on that are outside our control will not be our responsibility.</p ><h3>Image Policy</h3> <p>
+                            Unless a customer is willing to pay for a specific image, Rebel Design Solution integrates streaming pictures into their work. For customers, we utilize legally appropriate photos. We never utilize images backed by copyright protection.</p ><h2>Changes To Our Terms And Conditions Page</h2> <p>
+                            This page will be adjusted accordingly if we update, amend, or change its terms and conditions page. Users should revisit this page regularly to remain current on our policies.
+                            We are not legally responsible for updating clients regarding terms and conditions changes.
+                        </p >
+                        <h3>Rebel Design Solution's Images Policy</h3>
+                        <p>
+                            Every image on the Rebel Design Solution website is our property. No one may use these photographs in any way without first obtaining our written or verbal permission.
+                        </p >
+                        <h3>Privacy Policy</h3>
+                        <p>
+                            We request our clients review our Privacy Policy page to learn more about us.
+                        </p >
+                    </div >
                     <div className="col-md-2"></div>
-                </div>
-            </div>
+                </div >
+            </div >
         </>
     )
 }
